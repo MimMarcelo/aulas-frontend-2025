1ad2f767826c4058f78248c8a059e198c2e5c117
Marcelo Júnior <rokermarcelo@gmail.com>
2025-09-26 13:01:29 -0300

    Adicionada 3ª sessão com o uso de função anônima e passagem de parâmetros

--- 02.javascript-introdução/eventos.js
+++ 02.javascript-introdução/eventos.js
@@ -15,3 +15,29 @@ btnGravarNome.onclick = gravarNome;
 function gravarNome(){
     nome.innerText = campoNome.value
 }
+////////////////////////////////////
+
+//Mudar valor de atributo de elemento HTML => Versão Limitada
+// btnMudarAtributo1.onclick = mudarAtributo;
+// btnMudarAtributo2.onclick = mudarAtributo;
+
+// function mudarAtributo(tipo){
+//     if(tipo == 1){
+//         campoMudarAtributo.placeholder = "Login"
+//         return;
+//     }
+//     campoMudarAtributo.placeholder = "Senha"
+// }
+////////////////////////////////////
+
+//Mudar valor de atributo de elemento HTML
+btnMudarAtributo1.onclick = () => mudarAtributo(1);
+btnMudarAtributo2.onclick = () => mudarAtributo(2);
+
+function mudarAtributo(tipo){
+    if(tipo == 1){
+        campoMudarAtributo.placeholder = "Login"
+        return;
+    }
+    campoMudarAtributo.placeholder = "Senha"
+}
